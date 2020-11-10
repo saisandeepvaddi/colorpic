f793d5769aaba5554f9f0d055bab45630c4adcea
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-11-10 11:09:18 -0500

    use html instead of pug

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var pictures = require("./routes/pictures");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,14 +24,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", pictures);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
